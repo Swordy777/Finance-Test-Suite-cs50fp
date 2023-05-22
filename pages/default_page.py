79850ede4da7dfff3fc89d431121711305359246
Swordy777 <swordy777@gmail.com>
2023-05-22 14:53:07 +0200

    Reworked default page tests to be more isolated and rely only on webpage data; deleted database checks

--- pages/default_page.py
+++ pages/default_page.py
@@ -2,28 +2,31 @@ from .base_page import BasePage
 from .locators import DefaultPageLocators
 
 class DefaultPage(BasePage):
-    def get_stocktable(self):
+    def stocktable(self):
         return self.retrieve_element_if_present(*DefaultPageLocators.SHARES_TABLE)
     
-    def get_stocktable_headers(self):
+    def stocktable_headers(self):
         return self.retrieve_multiple_elements_if_present(*DefaultPageLocators.SHARES_TABLE_HEADERS)
     
-    def get_stocktable_rows(self):
+    def headers_names(self):
+        return [header.text for header in self.stocktable_headers()]
+    
+    def stocktable_rows(self):
         return self.retrieve_multiple_elements_if_present(*DefaultPageLocators.SHARES_TABLE_ROWS)
     
-    def get_stocktable_cells(self):
+    def stocktable_cells(self):
         cells = self.retrieve_multiple_elements_if_present(*DefaultPageLocators.SHARES_TABLE_ROW_CELLS)
-        headers = self.get_stocktable_headers()
+        headers = self.stocktable_headers()
         return self.organize_cell_data(cells, headers)
 
-    def get_cash_cell(self):
-        return self.retrieve_element_if_present(*DefaultPageLocators.SHARES_TABLE_CASH) 
+    def cash_element(self):
+        return self.retrieve_element_if_present(*DefaultPageLocators.CASH_ELEMENT) 
 
-    def get_total_cell(self):
-        return self.retrieve_element_if_present(*DefaultPageLocators.SHARES_TABLE_TOTAL)
+    def total_element(self):
+        return self.retrieve_element_if_present(*DefaultPageLocators.TOTAL_ELEMENT)
     
-    def get_cash_value(self):
-        return self.convert_currency_to_number(self.get_cash_cell().text)
+    def cash_elm_value(self):
+        return self.convert_currency_to_number(self.cash_element().text)
     
-    def get_total_value(self):
-        return self.convert_currency_to_number(self.get_total_cell().text)
+    def total_elm_value(self):
+        return self.convert_currency_to_number(self.total_element().text)
--- test_default_page.py
+++ test_default_page.py
@@ -4,8 +4,6 @@ from pages.buy_page import BuyPage
 from pages.sell_page import SellPage
 from pages.urls import URLS
 
-OLD_USER = "swordy1"
-OLD_PASSWORD = "123"
 DT_HEADER_SYMBOL = "Symbol"
 DT_HEADER_CNAME = "Name"
 DT_HEADER_AMOUNT = "Shares"
@@ -25,196 +23,145 @@ class TestDefaultPageBasicsAuthed():
     def test_has_stock_table(self, browser):
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        stock_table = dft_page.get_stocktable()
+        stock_table = dft_page.stocktable()
         assert stock_table is not None, "Expected for logged in user to see a table of purchased stocks"
 
-    def test_stocktable_has_correct_headers(self, browser):
+    def test_stocktable_has_correct_header_amount_and_names(self, browser):
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        table_headers = dft_page.get_stocktable_headers()
+        table_headers = dft_page.stocktable_headers()
         assert table_headers is not None, "Expected for logged in user to see stock table headers"
-        table_headers = [header.text for header in table_headers]
-        assert len(table_headers) == len(EXPECTED_HEADERS), (
-            f"Expected the stock table to have {len(EXPECTED_HEADERS)} columns {EXPECTED_HEADERS}; actual number of columns: {len(table_headers)}")
-        for theader, ex_header in zip(table_headers, EXPECTED_HEADERS):
-            assert theader == ex_header, f"Expected  table header {theader} to have a name {ex_header}"
+        header_names = dft_page.headers_names()
+        number_of_headers = len(table_headers)
+        ex_number_of_headers = len(EXPECTED_HEADERS)
+        assert number_of_headers == ex_number_of_headers, (
+            f"Expected the stock table to have {ex_number_of_headers} headers: {EXPECTED_HEADERS}; actual number of headers: {number_of_headers}")
+        for header, ex_header in zip(header_names, EXPECTED_HEADERS):
+            assert header == ex_header, f"Expected  table header {header} to have a name {ex_header}"
 
-    def test_stocktable_has_cash_cell(self, browser):
+    def test_has_cash_info(self, browser):
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        cash = dft_page.get_cash_cell()
+        cash = dft_page.cash_element()
         assert cash is not None, "Expected for logged in user to see an element containing current Cash amount"
 
-    def test_stocktable_has_total_cell(self, browser):
+    def test_has_total_info(self, browser):
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        total = dft_page.get_total_cell()
+        total = dft_page.total_element()
         assert total is not None, "Expected for logged in user to see an element containing TOTAL amount of money"
 
+@pytest.mark.usefixtures("new_user")
 class TestDefaultPageTable():
-    def test_new_user_table_has_no_data(self, browser, new_user):
+    def test_new_user_table_has_no_data(self, browser):
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        stock_rows = dft_page.get_stocktable_rows()
+        stock_rows = dft_page.stocktable_rows()
         assert stock_rows is None, "Expected stock table to have no rows of purchased stocks for newly registered user"
 
-    def test_new_user_has_10k_cash(self, browser, new_user):
+    def test_new_user_has_10k_cash(self, browser):
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        cash = dft_page.get_cash_value()
-        assert cash == dft_page.INITIAL_CASH, f"Expected total value to be 10,000.00, actual value: {cash} for newly registered user"
+        cash = dft_page.cash_elm_value()
+        assert cash == dft_page.INITIAL_CASH, f"Expected total value to be {dft_page.INITIAL_CASH}, actual value: {cash} for newly registered user"
 
-    def test_new_user_has_10k_total(self, browser, new_user):
+    def test_new_user_has_10k_total(self, browser):
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        total = dft_page.get_total_value()
-        assert total == dft_page.INITIAL_CASH, f"Expected TOTAL value to be 10,000.00, actual value: {total} for newly registered user"
+        total = dft_page.total_elm_value()
+        assert total == dft_page.INITIAL_CASH, f"Expected TOTAL value to be {dft_page.INITIAL_CASH}, actual value: {total} for newly registered user"
 
     @pytest.mark.parametrize("stock_symbols, stock_amounts", [(["NFLX", "AAPL"], [12, 6])])
-    def test_table_has_no_data_if_purchased_and_sold(self, browser, stock_symbols, stock_amounts, database, new_user):
+    def test_table_has_no_data_if_purchased_and_sold(self, browser, stock_symbols, stock_amounts):
         buy_page = BuyPage(browser, URLS.BUY_URL)
         for symbol, amount in zip(stock_symbols, stock_amounts):
             buy_page.open()
             buy_page.buy_stock(symbol, amount)
-            buy_page.query(database, 
-                        """
-                        insert into purchases (user_id, stockname, price, amount) 
-                        values ((select id from users where username = ?), ?, ?, ? )
-                        """, 
-                        new_user['username'], symbol, buy_page.lookup(symbol)['price'], amount)
         sell_page = SellPage(browser, URLS.SELL_URL)
         for symbol, amount in zip(stock_symbols, stock_amounts):
             sell_page.open()
             sell_page.sell_stock(symbol, amount)
-            buy_page.query(database, 
-                        """
-                        insert into purchases (user_id, stockname, price, amount) 
-                        values ((select id from users where username = ?), ?, ?, ? )
-                        """, 
-                        new_user['username'], symbol, buy_page.lookup(symbol)['price'], amount * (-1))
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        stock_rows = dft_page.get_stocktable_rows()
+        stock_rows = dft_page.stocktable_rows()
         assert stock_rows is None, (
-            "Expected stock table to have no rows of data if user sold all posessed stocks")
-        posessed_stocks = dft_page.query(database, 
-                                  """
-                                  select stockname from purchases p join users u on p.user_id = u.id 
-                                  where u.username=? group by p.stockname having sum(p.amount) > 0;
-                                  """,
-                                  new_user['username'])
-        assert posessed_stocks is None, (
-            "Expected the sum of user's stocks in database to equal to zero after selling everything")
+            "Expected stock table to have no rows of data if user sold all their stocks")
 
-    @pytest.mark.parametrize("stock_symbol, stock_amount", [("NFLX", 12)])
-    def test_cash_decreases_after_purchasing(self, new_user, browser, database, stock_symbol, stock_amount):
+    #TODO: add more parameters
+    @pytest.mark.parametrize("stock_symbol, stock_amount", [("NFLX", 12),
+                                                            ("AAPL", 7),
+                                                            ("MSFT", 4)])
+    def test_cash_decreases_after_purchasing(self, browser, stock_symbol, stock_amount):
         buy_page = BuyPage(browser, URLS.BUY_URL)
         buy_page.open()
         buy_page.buy_stock(stock_symbol, stock_amount)
-        # Insert the purchase into mock database
-        buy_page.query(database, 
-                    """
-                    insert into purchases (user_id, stockname, price, amount) 
-                    values ((select id from users where username = ?), ?, ?, ? )
-                    """, 
-                    new_user['username'], stock_symbol, buy_page.lookup(stock_symbol)['price'], stock_amount)
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        cash = dft_page.get_cash_value()
-        table_cells = dft_page.get_stocktable_cells()
+        cash = dft_page.cash_elm_value()
+        table_cells = dft_page.stocktable_cells()
         cash_spent = table_cells[DT_HEADER_PRICE] * table_cells[DT_HEADER_AMOUNT]
-        assert cash == dft_page.INITIAL_CASH - cash_spent, (
-            f"Expected cash to equal initial cash ({dft_page.INITIAL_CASH}) - stock price ({cash_spent}); actual value: {cash}")
-        db_cash = dft_page.query(database, "select cash from users where username = ?;", new_user['username'])
-        assert cash == db_cash['cash'], f"Expected total cash to equal {db_cash['cash']}, actual amount: {cash}" 
+        assert cash == round(dft_page.INITIAL_CASH - cash_spent, 2), (
+            f"Expected cash to equal initial cash - stock price ({dft_page.INITIAL_CASH - cash_spent}); actual value: {cash}")
 
-    @pytest.mark.parametrize("stock_symbol, stock_amount", [("NFLX", 12)])
-    def test_cash_increases_after_selling(self, new_user, browser, database, stock_symbol, stock_amount):
+    #TODO: add more parameters
+    @pytest.mark.parametrize("stock_symbol, stock_amount", [("NFLX", 12),
+                                                            ("AAPL", 7),
+                                                            ("MSFT", 4)])
+    def test_cash_increases_after_selling(self, browser, stock_symbol, stock_amount):
         buy_page = BuyPage(browser, URLS.BUY_URL)
         buy_page.open()
         buy_page.buy_stock(stock_symbol, stock_amount)
-        buy_page.query(database, 
-                    """
-                    insert into purchases (user_id, stockname, price, amount) 
-                    values ((select id from users where username = ?), ?, ?, ? )
-                    """, 
-                    new_user['username'], stock_symbol, buy_page.lookup(stock_symbol)['price'], stock_amount)
+        dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
+        dft_page.open()
+        cash_after_buying = dft_page.cash_elm_value()
         sell_page = SellPage(browser, URLS.SELL_URL)
         sell_page.open()
         sell_page.sell_stock(stock_symbol, stock_amount)
-        sell_page.query(database, 
-                    """
-                    insert into purchases (user_id, stockname, price, amount) 
-                    values ((select id from users where username = ?), ?, ?, ? )
-                    """, 
-                    new_user['username'], stock_symbol, buy_page.lookup(stock_symbol)['price'], stock_amount * (-1))
+        cash_earned = sell_page.lookup(stock_symbol)['price'] * stock_amount
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        cash = dft_page.get_cash_value()
-        db_cash = dft_page.query(database, "select cash from users where username = ?", new_user['username'])
-        assert cash == db_cash['cash'], (
-            f"Expected Default page table cash value ({cash}) to equal database cash value ({db_cash})")
+        cash = dft_page.cash_elm_value()
+        assert cash == cash_after_buying + cash_earned, (
+            f"Expected Default page table cash value ({cash}) to equal: {cash_after_buying + cash_earned}")
 
     @pytest.mark.parametrize("stock_symbols, stock_amounts", [(["NFLX", "AAPL"], [12, 6])])
-    def test_total_equals_cash_and_stock(self, new_user, stock_symbols, stock_amounts, browser, database):
+    def test_total_equals_cash_and_stock(self, stock_symbols, stock_amounts, browser):
         buy_page = BuyPage(browser, URLS.BUY_URL)
         for symbol, amount in zip(stock_symbols, stock_amounts):
             buy_page.open()
             buy_page.buy_stock(symbol, amount)
-            buy_page.query(database, 
-                        """
-                        insert into purchases (user_id, stockname, price, amount) 
-                        values ((select id from users where username = ?), ?, ?, ? )
-                        """, 
-                        new_user['username'], symbol, buy_page.lookup(symbol)['price'], amount)
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        total = dft_page.get_total_value()
-        cash = dft_page.get_cash_value()
-        table_rows = dft_page.get_stocktable_cells()
+        total_after_buying = dft_page.total_elm_value()
+        cash_after_buying = dft_page.cash_elm_value()
+        table_rows = dft_page.stocktable_cells()
+        stock_overall_price = 0
         for row in table_rows:
-            cash += row[DT_HEADER_PRICE] * row[DT_HEADER_AMOUNT]
-        assert total == cash, (
-            f"Expected total to equal the sum of leftover cash + stock cost, which equals {cash}; actual value: {total}")       
+            stock_overall_price += row[DT_HEADER_PRICE] * row[DT_HEADER_AMOUNT]
+        assert total_after_buying == cash_after_buying + stock_overall_price, (
+            f"Expected total to equal the sum of leftover cash + stock ({cash_after_buying + stock_overall_price}); actual value: {total}")       
         sell_page = SellPage(browser, URLS.SELL_URL)
         for symbol, amount in zip(stock_symbols, stock_amounts):
             sell_page.open()
             sell_page.sell_stock(symbol, amount)
-            sell_page.query(database, 
-                        """
-                        insert into purchases (user_id, stockname, price, amount) 
-                        values ((select id from users where username = ?), ?, ?, ? )
-                        """, 
-                        new_user['username'], symbol, buy_page.lookup(symbol)['price'], amount * (-1))
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        total = dft_page.get_total_value()
-        cash = dft_page.get_cash_value()
-        stock_profit = dft_page.query(database, 
-                                      f"""
-                                      select sum(amount)*sum(price) as stock_profit from purchases 
-                                      where user_id in (Select id from users where username=?);
-                                      """, 
-                                      new_user['username'])
-        cash = dft_page.query(database, "select cash from users where username = ?;", OLD_USER)
-        dbtotal = cash['cash'] + stock_profit['stock_profit']
-        assert total == dbtotal, f"Expected total to equal leftover cash + stock selling profits, which equals {dbtotal}; actual value: {total}" 
+        total = dft_page.total_elm_value()
+        cash = dft_page.cash_elm_value()
+        assert total == cash, f"Expected total to equal cash earned from selling stocks, which equals {cash}; actual value: {total}" 
 
-    @pytest.mark.parametrize("stock_symbol, stock_amount", [("NFLX", 12)])
-    def test_stock_total_should_equal_amount_x_price(self, new_user, stock_symbol, stock_amount, browser, database):
+    @pytest.mark.parametrize("stock_symbol, stock_amount", [("NFLX", 12),
+                                                            ("AAPL", 7),
+                                                            ("MSFT", 4)])
+    def test_stock_total_should_equal_amount_x_price(self, stock_symbol, stock_amount, browser):
         buy_page = BuyPage(browser, URLS.BUY_URL)
         buy_page.open()
         buy_page.buy_stock(stock_symbol, stock_amount)
-        buy_page.query(database, 
-                    """
-                    insert into purchases (user_id, stockname, price, amount) 
-                    values ((select id from users where username = ?), ?, ?, ? )
-                    """, 
-                    new_user['username'], stock_symbol, buy_page.lookup(stock_symbol)['price'], stock_amount)
         dft_page = DefaultPage(browser, URLS.DEFAULT_URL)
         dft_page.open()
-        stock = dft_page.get_stocktable_cells()
+        stock = dft_page.stocktable_cells()
         amount_x_price = round(stock[DT_HEADER_PRICE] * stock[DT_HEADER_AMOUNT], 2)
-        # print(f"\nPrice is {stock[DT_HEADER_PRICE]}; Amount is {stock[DT_HEADER_AMOUNT]}; Multiplied is {amount_x_price}; Table value is {stock[DT_HEADER_TOTAL]}")
         assert amount_x_price == stock[DT_HEADER_TOTAL], (
             f"Expected stock's {stock[DT_HEADER_SYMBOL]} total amount to equal {amount_x_price}, actual value: {stock[DT_HEADER_TOTAL]}")
+
+
