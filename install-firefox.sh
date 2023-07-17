104ae02a6b3781c5c34999a5c8a465c0e41ab871
Swordy777 <59532784+Swordy777@users.noreply.github.com>
2023-07-17 16:19:52 +0000

    Added scripts for browser installation

---
+++ install-firefox.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+echo "Changing to home directory..."
+pushd "$HOME"
+
+echo "Update the repository and any packages..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing libdbus-glib-1-2 which is necessary for Firefox..."
+sudo apt-get install libdbus-glib-1-2
+
+echo "Download the latest Firefox installation package..."
+wget -O ~/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+
+echo "Install Mozilla Firefox..."
+sudo tar xvjf ~/FirefoxSetup.tar.bz2 --directory /etc/alternatives/
+
+echo "Deleting the installation package..."
+rm FirefoxSetup.tar.bz2
+
+echo "Creating symbolic link for the unpacked Firefox..."
+sudo ln -s /etc/alternatives/firefox/firefox /usr/bin/firefox
+
+firefox_version=($(firefox --version))
+echo "Firefox version: ${firefox_version[2]}"
+
+popd
